562bfcd57842f611a79ea76ee7ccaf2de9047c0f
Kenichi Sasaki <sasaki@lss.mes.titech.ac.jp>
2018-12-15 13:51:26 +0900

    add schedule

--- lep_prototype/src/views/Register/index.vue
+++ lep_prototype/src/views/Register/index.vue
@@ -1,8 +1,23 @@
 <template>
   <div class="home">
+    <v-data-table
+    :headers="headers"
+    :items="time"
+    class="elevation-1"
+  >
+    <template slot="items" slot-scope="props">
+      <td>{{ props.item.name }}</td>
+      <td><v-checkbox　v-model="props.item.mondayValue"></v-checkbox></td>
+      <td><v-checkbox　v-model="props.item.tuedayValue"></v-checkbox></td>
+      <td><v-checkbox　v-model="props.item.wednesdayValue"></v-checkbox></td>
+      <td><v-checkbox　v-model="props.item.thursdayValue"></v-checkbox></td>
+      <td><v-checkbox　v-model="props.item.fridayValue"></v-checkbox></td>
+
+      
+    </template>
+  </v-data-table>
     <v-content>
       <v-container>
-        
         <v-layout>
           <v-flex xs12>
             <img width="180px" alt="Logo" src="../../assets/LEP_logo.png">
@@ -13,13 +28,12 @@
 
       <v-container>
         <v-layout row>
-
           <v-flex xs8>
             <v-card flat>
               <v-card-text>
-              <HelloWorld/>
+                <HelloWorld/>
               </v-card-text>
-             </v-card>
+            </v-card>
            </v-flex>
 
           <v-flex xs4>
@@ -61,7 +75,78 @@ export default {
   data() {
       return {
         email: null,
-        password: null
+        password: null,
+        headers:[
+          {
+            text: 'TIMELINE',
+            align: 'left',
+            sortable: false,
+            value: 'name'
+          },
+          { text: 'Mon', value: 'mon' },
+          { text: 'Tue', value: 'tue' },
+          { text: 'Wed', value: 'wed' },
+          { text: 'Thu', value: 'thu' },
+          { text: 'Fri', value: 'Fri' }
+        ],
+        time: [
+          {
+            mondayValue: false,
+            tuedayValue: false,
+            wednesdayValue: false,
+            thursdayValue: false,
+            fridayValue: false,
+            name: '9:00~10:30'
+          },
+          {
+            mondayValue: false,
+            tuedayValue: false,
+            wednesdayValue: false,
+            thursdayValue: false,
+            fridayValue: false,
+            name: '10:45~12:15'
+          },
+          {
+            mondayValue: false,
+            tuedayValue: false,
+            wednesdayValue: false,
+            thursdayValue: false,
+            fridayValue: false,
+            name: 'Lunch Time'
+          },
+          {
+            mondayValue: false,
+            tuedayValue: false,
+            wednesdayValue: false,
+            thursdayValue: false,
+            fridayValue: false,
+            name: '13:20~14:50'
+          },
+          {
+            mondayValue: false,
+            tuedayValue: false,
+            wednesdayValue: false,
+            thursdayValue: false,
+            fridayValue: false,
+            name: '15:05~16:35'
+          },
+          {
+            mondayValue: false,
+            tuedayValue: false,
+            wednesdayValue: false,
+            thursdayValue: false,
+            fridayValue: false,
+            name: '16:50~18:20'
+          },
+          {
+            mondayValue: false,
+            tuedayValue: false,
+            wednesdayValue: false,
+            thursdayValue: false,
+            fridayValue: false,
+            name: '18:35~'
+          }
+        ]
       }
   },
 
