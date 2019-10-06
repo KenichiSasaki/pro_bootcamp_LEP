cfd20926b9c8c9ec5b1d089e912f2d2dca08c4de
yoheikoizumi <koizumi.y.ac@m.titech.ac.jp>
2019-10-06 13:37:43 +0900

    fix firebase config

--- lep_prototype/src/main.js
+++ lep_prototype/src/main.js
@@ -8,12 +8,13 @@ import { convertDateToString } from "@/filters/date-filter";
 import firebase from "firebase";
 
 var config = {
-  apiKey: "AIzaSyCZz6ShA6t0zt2ZjDdKI3pWly33Yyb4cGA",
+  apiKey: "AIzaSyDF41xNsjDukv0sYTx3jN3mYrYeMQfb5j0",
   authDomain: "leptitechfirebase.firebaseapp.com",
   databaseURL: "https://leptitechfirebase.firebaseio.com",
   projectId: "leptitechfirebase",
   storageBucket: "leptitechfirebase.appspot.com",
-  messagingSenderId: "913775785175"
+  messagingSenderId: "913775785175",
+  appId: "1:913775785175:web:4b281266bbc0a570366c92"
 };
 firebase.initializeApp(config);
 
